935100c506fd097404b8f61cfe98def69e5f9cda
Ivan <ivan.goister@gmail.com>
2021-05-28 00:11:29 +0300

    First things about working algoritm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 equation = input('Введіть рівняння: ')
+equation = equation.replace(' ', '')
 # Отримувати рівняння як стороку
 # Видалити зайві пробіли
 # Визначити позиції з знаками операції
@@ -9,3 +10,38 @@ equation = input('Введіть рівняння: ')
 #~~~~~~~~~~~~~~
 
 # Логіка визначення виразу в дужках
+
+symbols = [ '+', '-', '*', '/', '(', ')']
+symbols_loc = []
+loc_plus = []
+loc_minus = []
+loc_multi = []
+loc_divide = []
+loc_open = []
+loc_close = []
+numbers = []
+
+for i in range len(equation):
+    if equation[i] in symbols:
+        symbols_loc.append(i)
+    else:
+        pass
+
+def symbols_location():
+    for i in range len(equation):
+        if equation[i] == "(":
+           loc_open.append(i)
+        elif equation[i] == ")":
+            loc_close.append(i)
+        elif equation[i] == "+":
+            loc_plus.append(i)
+        elif equation[i] == "-":
+            loc_minus.append(i)
+        elif equation[i] == "*":
+            loc_multi.append(i)
+        elif equation[i] == "/":
+            loc_divide.append(i)
+        else:
+            pass
+
+symbols_location()
