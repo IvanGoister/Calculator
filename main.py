6fc3504f427cc7c9670907f78e38b97d7ebf9444
Ivan <ivan.goister@gmail.com>
2021-05-30 16:02:26 +0300

    Calculation is working. For now only use '+' '-' '*' '/'  symbols, and first number must be a positive

--- main.py
+++ main.py
@@ -2,17 +2,10 @@
 equation = input('Введіть рівняння: ')
 equation = equation.replace(' ', '')
 len_equation=len(equation)
-# Отримувати рівняння як стороку +
-# Видалити зайві пробіли +
-# Визначити позиції з знаками операції +
-# Розділити числа по окремим змінним
-# Пріоритети виконання операцій
-print(equation)
-print(len(equation))
-#~~~~~~~~~~~~~~
+new_list=[]
 
 # Логіка визначення виразу в дужках
-
+answer=0
 symbols = [ '+', '-', '*', '/', '(', ')']
 symbols_loc = []
 loc_plus = []
@@ -50,7 +43,7 @@ def symbols_location():
 symbols_location()
 
 #   1+2*34+5/6+789
-
+#   5+8*2-4*4+16-32/2
 def create_numbers():
     last_i = symbols_loc[0]
     numbers.append(int(equation[0:last_i]))
@@ -71,11 +64,59 @@ def create_numbers():
     return numbers
 create_numbers()
 
-print('symbols_loc ',  symbols_loc)
-print('loc_plus ',  loc_plus)
-print('loc_minus ', loc_minus)
-print('loc_multi ', loc_multi)
-print('loc_divide ', loc_divide)
-print('loc_open ', loc_open)
-print('loc_close ', loc_close)
-print('numbers ', numbers)
+#print('symbols_loc ',  symbols_loc)
+#print('loc_plus ',  loc_plus)
+#print('loc_minus ', loc_minus)
+#print('loc_multi ', loc_multi)
+#print('loc_divide ', loc_divide)
+#print('loc_open ', loc_open)
+#print('loc_close ', loc_close)
+#print('numbers ', numbers)
+
+def create_new_list():
+
+    for i in range (0, len(symbols_loc)):
+        new_list.append(numbers[i])
+        new_list.append(equation[symbols_loc[i]])
+    new_list.append(numbers[-1])
+    #print(new_list)
+    return new_list
+
+create_new_list()
+
+def calculation():
+    i = 0
+    len_new_list=len(new_list)
+    while i < len_new_list:
+        if new_list[i] == '*':
+            new_numb = new_list[i-1] * new_list[i+1]
+            new_list[i - 1:i + 2] = [new_numb]
+            len_new_list = len(new_list)
+            i = i - 1
+        elif new_list[i] == '/':
+            new_numb = new_list[i - 1] / new_list[i + 1]
+            new_list[i - 1:i + 2] = [new_numb]
+            len_new_list = len(new_list)
+            i = i - 1
+        else:
+            i = i + 1
+
+    i = 0
+    len_new_list = len(new_list)
+    while i < len_new_list:
+        if new_list[i] == '+':
+            new_numb = new_list[i - 1] + new_list[i + 1]
+            new_list[i - 1:i + 2] = [new_numb]
+            len_new_list = len(new_list)
+            i = i - 1
+        elif new_list[i] == '-':
+            new_numb = new_list[i - 1] - new_list[i + 1]
+            new_list[i - 1:i + 2] = [new_numb]
+            len_new_list = len(new_list)
+            i = i - 1
+        else:
+            i = i + 1
+    answer=new_list[0]
+    print('Відповідь: ',equation, "=", answer)
+    return answer
+calculation()
